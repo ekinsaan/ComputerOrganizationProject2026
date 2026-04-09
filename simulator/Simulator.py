6993ddad867a9ca06ff6f52392b43c46f7d7fbc9
shreyash-IIITD <shreyashkanaujia1@gmail.com>
2026-04-09 23:28:38 +0530

    added execute functions for S,B,U,J types

--- simulator/Simulator.py
+++ simulator/Simulator.py
@@ -63,6 +63,82 @@ def Execute_R_Type(Decoded):
             Registers[rd] = (rs1 + rs2) & 0xFFFFFFFF
             ProgramCounter += 4
 
+def Execute_S_Type(Decoded):
+    global ProgramCounter
+    rs1 = Registers[Decoded["rs1"]]
+    rs2 = Registers[Decoded["rs2"]]
+    imm = SignedBinaryToDecimal(Decoded["imm"])
+
+    if Decoded["funct3"] == "010":
+        # sw
+        address = (rs1 + imm) & 0xFFFFFFFF
+        MemoryWrite(address, rs2)
+    ProgramCounter += 4
+
+def Execute_B_Type(Decoded):
+    global ProgramCounter
+    rs1 = Registers[Decoded["rs1"]]
+    rs2 = Registers[Decoded["rs2"]]
+    imm = SignedBinaryToDecimal(Decoded["imm"])
+
+    signed_rs1 = rs1 if rs1 < 0x80000000 else rs1 - 0x100000000
+    signed_rs2 = rs2 if rs2 < 0x80000000 else rs2 - 0x100000000
+
+    branch = False
+
+    if Decoded["funct3"] == "000":
+        # beq
+        branch = (rs1 == rs2)
+
+    elif Decoded["funct3"] == "001":
+        # bne
+        branch = (rs1 != rs2)
+
+    elif Decoded["funct3"] == "100":
+        # blt
+        branch = (signed_rs1 < signed_rs2)
+
+    elif Decoded["funct3"] == "101":
+        # bge
+        branch = (signed_rs1 >= signed_rs2)
+
+    elif Decoded["funct3"] == "110":
+        # bltu
+        branch = (rs1 < rs2)
+
+    elif Decoded["funct3"] == "111":
+        # bgeu
+        branch = (rs1 >= rs2)
+
+    if branch:
+        ProgramCounter = (ProgramCounter + imm) & 0xFFFFFFFF
+    else:
+        ProgramCounter += 4
+
+def Execute_U_Type(Decoded):
+    global ProgramCounter
+    rd  = Decoded["rd"]
+    imm = int(Decoded["imm"], 2) << 12
+
+    if Decoded["opcode"] == "0110111":
+        # lui
+        Registers[rd] = imm & 0xFFFFFFFF
+        ProgramCounter += 4
+
+    elif Decoded["opcode"] == "0010111":
+        # auipc
+        Registers[rd] = (ProgramCounter + imm) & 0xFFFFFFFF
+        ProgramCounter += 4
+
+def Execute_J_Type(Decoded):
+    global ProgramCounter
+    rd  = Decoded["rd"]
+    imm = SignedBinaryToDecimal(Decoded["imm"])
+
+    # jal
+    Registers[rd]  = (ProgramCounter + 4) & 0xFFFFFFFF
+    ProgramCounter = (ProgramCounter + imm) & 0xFFFFFFFF
+
 def Execute(decoded):
     if decoded["type"] == "R":
         Execute_R_Type(decoded)
